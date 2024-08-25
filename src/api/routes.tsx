5c213419035e94e998fa11c5ea31c6f9473d2650
ibnuputraard <ibnu.putra@nawadata.id>
2024-08-25 17:22:50 +0700

    using meta env

--- src/api/routes.tsx
+++ src/api/routes.tsx
@@ -1,5 +1,4 @@
-export const BASE_URL =
-  'https://anggrekapi.my.id/'; /*'http://localhost:8080/'*/
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 // PATH
 export const LOGIN = 'admin/login';
